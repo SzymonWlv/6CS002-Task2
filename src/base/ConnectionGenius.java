d7c723236c306f1852debd9135f8f5c1a7fe2d6a
Szymon Baraniecki <S.K.Baraniecki@Wlv.ac.uk>
2023-07-08 14:12:39 +0100

    ConnectionGenius - Code Annotation

--- src/base/ConnectionGenius.java
+++ src/base/ConnectionGenius.java
@@ -3,33 +3,33 @@ package base;
 import java.net.InetAddress;
 
 /**
- * @author Kevan Buckley, maintained by __student
+ * @author Kevan Buckley, maintained by 1913153 Szymon Baraniecki student
  * @version 2.0, 2014
  */
-
+//Java class 'ConnectionGenius'
 public class ConnectionGenius {
-
+	// InetAddress class used to determine IP address assigned to variable 'ipa'
 	InetAddress ipa;
 
 	public ConnectionGenius(InetAddress ipa) {
 		this.ipa = ipa;
 	}
-
+	// 'fireUpGame' Method used to initialise game start up process
 	public void fireUpGame() {
-		downloadWebVersion();
-		connectToWebService();
-		awayWeGo();
+		downloadWebVersion(); // Used to download web version of the game - Including assets/resources etc.
+		connectToWebService(); // Used to establish a connection with a online server.
+		awayWeGo(); // Used to start the game
 	}
-
+	// Outputs generated whilst 'downloadWebVersion' method is started
 	public void downloadWebVersion() {
 		System.out.println("Getting specialised web version.");
 		System.out.println("Wait a couple of moments");
 	}
-
+	// Output generated whilst 'connectToWebService' method is started
 	public void connectToWebService() {
 		System.out.println("Connecting");
 	}
-
+	// Output generated whilst 'awayWeGo' method is started
 	public void awayWeGo() {
 		System.out.println("Ready to play");
 	}
